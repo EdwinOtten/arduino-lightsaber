d1f506ba496c4d599ca55e92af55acb4145dc711
Edwin Otten <EdwinOtten@users.noreply.github.com>
2022-10-10 22:32:37 +0200

    Create master.ino

---
+++ master.ino
@@ -0,0 +1,80 @@
+#include <Wire.h>
+
+#define DEBUG 1
+
+#ifdef DEBUG
+  #define DEBUG_PRINT(x)  Serial.println (x)
+#else
+  #define DEBUG_PRINT(x)
+#endif
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+int btnState = 0;
+int btnCount = 0;
+long btnLastPressed = 0;
+
+byte mode = 0;
+
+
+void setup()
+{
+	if (DEBUG)
+    {
+		Serial.begin(9600);
+    }
+  	pinMode(buttonPin, INPUT);
+  	Wire.begin(); // join i2c bus (address optional for master)
+	DEBUG_PRINT("setup complete");
+}
+
+void loop()
+{
+  	int newState = digitalRead(buttonPin);
+  
+	if (newState != btnState)
+  	{
+    	btnState = newState;
+    	if (btnState == HIGH)
+    	{
+        	btnCount++;
+    		btnLastPressed = millis();
+    	}
+  	}
+  
+	if (btnCount > 0 && btnLastPressed < millis()-500)
+	{
+    	handleBtnPress();          		
+    	btnCount = 0;
+	}
+}
+
+void handleBtnPress()
+{
+    switch (btnCount)
+    {
+      case 1:
+        DEBUG_PRINT("single press");
+      	sendMode(1);
+        break;
+      case 2:
+        DEBUG_PRINT("double press");
+      	sendMode(2);
+        break;
+      case 3:
+        DEBUG_PRINT("triple press");
+      	sendMode(3);
+        break;
+      case 4:
+        DEBUG_PRINT("quad press");
+      	sendMode(4);
+        break;
+    }
+}
+
+void sendMode(byte mode)
+{
+	Wire.beginTransmission(4); // transmit to device #4
+	Wire.write(mode);          // sends one byte  
+	Wire.endTransmission();    // stop transmitting
+}
