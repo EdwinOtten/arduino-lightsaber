2976624800ec474604c377594b1364cae997a684
Edwin Otten <edwin.otten@sogeti.com>
2022-10-17 22:29:49 +0200

    Implemented colors + flickering

--- arduino-sketchbooks/led-controller/led-controller.ino
+++ arduino-sketchbooks/led-controller/led-controller.ino
@@ -3,7 +3,7 @@
 #define DEBUG 1
 
 #ifdef DEBUG
-#define DEBUG_PRINT(x) Serial.println(F(x))
+#define DEBUG_PRINT(x) Serial.println(x)
 #else
 #define DEBUG_PRINT(x)
 #endif
@@ -27,6 +27,16 @@ byte mode = 0;
 byte newMode = 0;
 bool newModeAvailable = false;
 
+// colors in R, G, B
+const uint8_t colors[5][3] = { 
+  { 10, 90, 180 }, // blue
+  { 180, 0, 0 }, // red
+  { 160, 0, 180 }, // purple
+  { 10, 200, 0 }, // green
+  { 180, 30, 0 }, // amber
+};
+uint8_t color[3] = { 0, 0, 0 };
+
 /*
  This is an example of how simple driving a Neopixel can be
  This code is optimized for understandability and changability rather than raw speed
@@ -34,9 +44,9 @@ bool newModeAvailable = false;
 */
 
 // Change this to be at least as long as your pixel string (too long will work fine, just be a little slower)
-#define PIXELS 144 + 1 // Number of pixels in the string
+#define PIXELS 144  // Number of pixels in the string
 
-// These values depend on which pin your string is connected to and what board you are using
+// These values depend on which pin your string is connected to and what board you are using 
 // More info on how to find these at http://www.arduino.cc/en/Reference/PortManipulation
 
 // These values are for the pin that connects to the Data Input pin on the LED strip. They correspond to...
@@ -45,178 +55,184 @@ bool newModeAvailable = false;
 // DueMilinove/UNO: Digital Pin 12
 // Arduino MeagL    PWM Pin 4
 
-// You'll need to look up the port/bit combination for other boards.
-// B for UNO
-// D for Nano
+// You'll need to look up the port/bit combination for other boards. 
+
 // Note that you could also include the DigitalWriteFast header file to not need to to this lookup.
 
-#define PIXEL_PORT PORTB // Port of the pin the pixels are connected to
-#define PIXEL_DDR DDRB   // Port of the pin the pixels are connected to
-#define PIXEL_BIT 4      // Bit of the pin the pixels are connected to
+#define PIXEL_PORT  PORTD  // Port of the pin the pixels are connected to
+#define PIXEL_DDR   DDRD   // Port of the pin the pixels are connected to
+#define PIXEL_BIT   6      // Bit of the pin the pixels are connected to
 
-// These are the timing constraints taken mostly from the WS2812 datasheets
-// These are chosen to be conservative and avoid problems rather than for maximum throughput
+// These are the timing constraints taken mostly from the WS2812 datasheets 
+// These are chosen to be conservative and avoid problems rather than for maximum throughput 
 
-#define T1H 900 // Width of a 1 bit in ns
-#define T1L 600 // Width of a 1 bit in ns
+#define T1H  900    // Width of a 1 bit in ns
+#define T1L  600    // Width of a 1 bit in ns
 
-#define T0H 400 // Width of a 0 bit in ns
-#define T0L 900 // Width of a 0 bit in ns
+#define T0H  400    // Width of a 0 bit in ns
+#define T0L  900    // Width of a 0 bit in ns
 
 // Older pixels would reliabily reset with a 6us delay, but some newer (>2019) ones
 // need 250us. This is set to the longer delay here to make sure things work, but if
 // you want to get maximum refresh speed, you can try decreasing this until your pixels
-// start to flicker.
+// start to flicker. 
 
-#define RES 250000 // Width of the low gap between bits to cause a frame to latch
+#define RES 250000    // Width of the low gap between bits to cause a frame to latch
 
 // Here are some convience defines for using nanoseconds specs to generate actual CPU delays
 
-#define NS_PER_SEC (1000000000L) // Note that this has to be SIGNED since we want to be able to check for negative values of derivatives
+#define NS_PER_SEC (1000000000L)          // Note that this has to be SIGNED since we want to be able to check for negative values of derivatives
 
 #define CYCLES_PER_SEC (F_CPU)
 
-#define NS_PER_CYCLE (NS_PER_SEC / CYCLES_PER_SEC)
+#define NS_PER_CYCLE ( NS_PER_SEC / CYCLES_PER_SEC )
 
-#define NS_TO_CYCLES(n) ((n) / NS_PER_CYCLE)
+#define NS_TO_CYCLES(n) ( (n) / NS_PER_CYCLE )
 
 // Actually send a bit to the string. We must to drop to asm to enusre that the complier does
 // not reorder things and make it so the delay happens in the wrong place.
 
-inline void sendBit(bool bitVal)
-{
-
-  if (bitVal)
-  { // 0 bit
-
-    asm volatile(
-        "sbi %[port], %[bit] \n\t" // Set the output bit
-        ".rept %[onCycles] \n\t"   // Execute NOPs to delay exactly the specified number of cycles
-        "nop \n\t"
-        ".endr \n\t"
-        "cbi %[port], %[bit] \n\t" // Clear the output bit
-        ".rept %[offCycles] \n\t"  // Execute NOPs to delay exactly the specified number of cycles
-        "nop \n\t"
-        ".endr \n\t" ::
-            [port] "I"(_SFR_IO_ADDR(PIXEL_PORT)),
-        [bit] "I"(PIXEL_BIT),
-        [onCycles] "I"(NS_TO_CYCLES(T1H) - 2), // 1-bit width less overhead  for the actual bit setting, note that this delay could be longer and everything would still work
-        [offCycles] "I"(NS_TO_CYCLES(T1L) - 2) // Minimum interbit delay. Note that we probably don't need this at all since the loop overhead will be enough, but here for correctness
+inline void sendBit( bool bitVal ) {
+  
+    if (  bitVal ) {        // 0 bit
+      
+    asm volatile (
+      "sbi %[port], %[bit] \n\t"        // Set the output bit
+      ".rept %[onCycles] \n\t"                                // Execute NOPs to delay exactly the specified number of cycles
+      "nop \n\t"
+      ".endr \n\t"
+      "cbi %[port], %[bit] \n\t"                              // Clear the output bit
+      ".rept %[offCycles] \n\t"                               // Execute NOPs to delay exactly the specified number of cycles
+      "nop \n\t"
+      ".endr \n\t"
+      ::
+      [port]    "I" (_SFR_IO_ADDR(PIXEL_PORT)),
+      [bit]   "I" (PIXEL_BIT),
+      [onCycles]  "I" (NS_TO_CYCLES(T1H) - 2),    // 1-bit width less overhead  for the actual bit setting, note that this delay could be longer and everything would still work
+      [offCycles]   "I" (NS_TO_CYCLES(T1L) - 2)     // Minimum interbit delay. Note that we probably don't need this at all since the loop overhead will be enough, but here for correctness
 
     );
-  }
-  else
-  { // 1 bit
+                                  
+    } else {          // 1 bit
 
     // **************************************************************************
     // This line is really the only tight goldilocks timing in the whole program!
     // **************************************************************************
 
-    asm volatile(
-        "sbi %[port], %[bit] \n\t" // Set the output bit
-        ".rept %[onCycles] \n\t"   // Now timing actually matters. The 0-bit must be long enough to be detected but not too long or it will be a 1-bit
-        "nop \n\t"                 // Execute NOPs to delay exactly the specified number of cycles
-        ".endr \n\t"
-        "cbi %[port], %[bit] \n\t" // Clear the output bit
-        ".rept %[offCycles] \n\t"  // Execute NOPs to delay exactly the specified number of cycles
-        "nop \n\t"
-        ".endr \n\t" ::
-            [port] "I"(_SFR_IO_ADDR(PIXEL_PORT)),
-        [bit] "I"(PIXEL_BIT),
-        [onCycles] "I"(NS_TO_CYCLES(T0H) - 2),
-        [offCycles] "I"(NS_TO_CYCLES(T0L) - 2)
+
+    asm volatile (
+      "sbi %[port], %[bit] \n\t"        // Set the output bit
+      ".rept %[onCycles] \n\t"        // Now timing actually matters. The 0-bit must be long enough to be detected but not too long or it will be a 1-bit
+      "nop \n\t"                                              // Execute NOPs to delay exactly the specified number of cycles
+      ".endr \n\t"
+      "cbi %[port], %[bit] \n\t"                              // Clear the output bit
+      ".rept %[offCycles] \n\t"                               // Execute NOPs to delay exactly the specified number of cycles
+      "nop \n\t"
+      ".endr \n\t"
+      ::
+      [port]    "I" (_SFR_IO_ADDR(PIXEL_PORT)),
+      [bit]   "I" (PIXEL_BIT),
+      [onCycles]  "I" (NS_TO_CYCLES(T0H) - 2),
+      [offCycles] "I" (NS_TO_CYCLES(T0L) - 2)
 
     );
-  }
+      
+    }
+    
+    // Note that the inter-bit gap can be as long as you want as long as it doesn't exceed the 5us reset timeout (which is A long time)
+    // Here I have been generous and not tried to squeeze the gap tight but instead erred on the side of lots of extra time.
+    // This has thenice side effect of avoid glitches on very long strings becuase 
 
-  // Note that the inter-bit gap can be as long as you want as long as it doesn't exceed the 5us reset timeout (which is A long time)
-  // Here I have been generous and not tried to squeeze the gap tight but instead erred on the side of lots of extra time.
-  // This has thenice side effect of avoid glitches on very long strings becuase
-}
+    
+}  
 
-inline void sendByte(unsigned char byte)
-{
-
-  for (unsigned char bit = 0; bit < 8; bit++)
-  {
-
-    sendBit(bitRead(byte, 7)); // Neopixel wants bit in highest-to-lowest order
-                               // so send highest bit (bit #7 in an 8-bit byte since they start at 0)
-    byte <<= 1;                // and then shift left so bit 6 moves into 7, 5 moves into 6, etc
-  }
-}
+  
+inline void sendByte( unsigned char byte ) {
+    
+    for( unsigned char bit = 0 ; bit < 8 ; bit++ ) {
+      
+      sendBit( bitRead( byte , 7 ) );                // Neopixel wants bit in highest-to-lowest order
+                                                     // so send highest bit (bit #7 in an 8-bit byte since they start at 0)
+      byte <<= 1;                                    // and then shift left so bit 6 moves into 7, 5 moves into 6, etc
+      
+    }           
+} 
 
 /*
 
   The following three functions are the public API:
-
-  ledSetup() - set up the pin that is connected to the string. Call once at the begining of the program.
+  
+  ledSetup() - set up the pin that is connected to the string. Call once at the begining of the program.  
   sendPixel( r g , b ) - send a single pixel to the string. Call this once for each pixel in a frame.
-  show() - show the recently sent pixel on the LEDs . Call once per frame.
-
+  show() - show the recently sent pixel on the LEDs . Call once per frame. 
+  
 */
 
+
 // Set the specified pin up as digital out
-void ledsetup()
-{
-  bitSet(PIXEL_DDR, PIXEL_BIT);
+
+void ledsetup() {
+  
+  bitSet( PIXEL_DDR , PIXEL_BIT );
+  
 }
 
-inline void sendPixel(unsigned char r, unsigned char g, unsigned char b)
-{
-  sendByte(g / 10);
-  sendByte(r / 10);
-  sendByte(b / 10);
-  // Neopixel wants colors in green then red then blue order
-  // My led strip wants colors in the order R G B
+inline void sendPixel( unsigned char r, unsigned char g , unsigned char b )  {  
+  
+  sendByte(r/10);
+  sendByte(g/10);
+  sendByte(b/10);          // Neopixel wants colors in green then red then blue order
+  
 }
+
 
 // Just wait long enough without sending any bots to cause the pixels to latch and display the last sent frame
-void show()
-{
-  _delay_us((RES / 1000UL) + 1); // Round up since the delay must be _at_least_ this long (too short might not work, too long not a problem)
+
+void show() {
+  _delay_us( (RES / 1000UL) + 1);       // Round up since the delay must be _at_least_ this long (too short might not work, too long not a problem)
 }
+
 
 /*
 
   That is the whole API. What follows are some demo functions rewriten from the AdaFruit strandtest code...
-
+  
   https://github.com/adafruit/Adafruit_NeoPixel/blob/master/examples/strandtest/strandtest.ino
-
+  
   Note that we always turn off interrupts while we are sending pixels becuase an interupt
   could happen just when we were in the middle of somehting time sensitive.
-
-  If we wanted to minimize the time interrupts were off, we could instead
-  could get away with only turning off interrupts just for the very brief moment
-  when we are actually sending a 0 bit (~1us), as long as we were sure that the total time
+  
+  If we wanted to minimize the time interrupts were off, we could instead 
+  could get away with only turning off interrupts just for the very brief moment 
+  when we are actually sending a 0 bit (~1us), as long as we were sure that the total time 
   taken by any interrupts + the time in our pixel generation code never exceeded the reset time (5us).
-
+  
 */
+
 
 // Display a single color on the whole string
 
-void showColor(unsigned char r, unsigned char g, unsigned char b)
-{
-
-  cli();
-  for (int p = 0; p < PIXELS; p++)
-  {
-    sendPixel(r, g, b);
+void showColor( unsigned char r , unsigned char g , unsigned char b ) {
+  
+  cli();  
+  for( int p=0; p<PIXELS; p++ ) {
+    sendPixel( r , g , b );
   }
   sei();
   show();
+  
 }
 
 void saberOn()
 {
   DEBUG_PRINT("Saber on");
-  colorWipe(10, 100, 120, 10);
+  colorWipe(5);
 }
 
 void saberOff()
 {
   DEBUG_PRINT("Saber off");
-  colorRetract(10, 100, 120, 10);
+  colorRetract(5);
 }
 
 void rainbow()
@@ -264,17 +280,23 @@ void rainbow()
 
 // Fill the dots one after the other with a color
 // rewrite to lift the compare out of the loop
-void colorWipe(unsigned char r, unsigned char g, unsigned char b, unsigned char wait)
+void colorWipe(unsigned char wait)
 {
+  float multiplier = getFlickerMultiplier();
   for (signed int i = 0; i < PIXELS; i++)
   {
-
-    cli();
     unsigned int p = 0;
+    if (i%10 == 0) {
+      multiplier = getFlickerMultiplier();
+    }
+    uint8_t r = min(255, color[0]*multiplier);
+    uint8_t g = min(255, color[1]*multiplier);
+    uint8_t b = min(255, color[2]*multiplier);
+    cli();
 
     while (p < i)
     {
-      sendPixel(r, g, b);
+      sendPixel(r,g,b);
       p++;
     }
 
@@ -290,27 +312,34 @@ void colorWipe(unsigned char r, unsigned char g, unsigned char b, unsigned char 
 
     if (newModeAvailable)
     {
+      DEBUG_PRINT("break out of colorWipe");
       return;
     }
   }
 }
 
 // Fill the dots one after the other with a color (reversed)
-void colorRetract(unsigned char r, unsigned char g, unsigned char b, unsigned char wait)
+void colorRetract(unsigned char wait)
 {
+  float multiplier = getFlickerMultiplier();
   for (signed int i = PIXELS - 1; i >= 0; i--)
   {
-
-    cli();
     unsigned int p = 0;
+    if (i%5 == 0) {
+      multiplier = getFlickerMultiplier();
+    }
+    uint8_t r = min(255, color[0]*multiplier);
+    uint8_t g = min(255, color[1]*multiplier);
+    uint8_t b = min(255, color[2]*multiplier);
+    cli();
 
     while (p < i)
     {
-      sendPixel(r, g, b);
+      sendPixel(r,g,b);
       p++;
     }
 
-    while (p < PIXELS - 1)
+    while (p < PIXELS)
     {
       sendPixel(0, 0, 0);
       p++;
@@ -327,27 +356,39 @@ void colorRetract(unsigned char r, unsigned char g, unsigned char b, unsigned ch
   }
 }
 
+float getFlickerMultiplier()
+{
+  bool saberFlickerDisabled = BIT_CHECK(mode, 6);
+  if (saberFlickerDisabled)
+    return 1.0f;
+  else
+    return random(9, 16) * 0.1f;
+}
+
 void saberLoop()
 {
   DEBUG_PRINT("Saber loop");
 
-  signed int spikes[] = {-1, -1, -1};
-
   while (!newModeAvailable)
   {
-    cli();
     unsigned int p = 0;
+    float multiplier = getFlickerMultiplier();
+    uint8_t r = min(255, color[0]*multiplier);
+    uint8_t g = min(255, color[1]*multiplier);
+    uint8_t b = min(255, color[2]*multiplier);
+    cli();
 
     while (p < PIXELS)
     {
-      sendPixel(10, 100, 120);
+      sendPixel(r,g,b);
       p++;
     }
 
     sei();
     show();
-    delay(10);
+    delay(50);
   }
+  DEBUG_PRINT("break out of saberLoop");
 }
 
 void setup()
@@ -355,11 +396,13 @@ void setup()
   if (DEBUG)
   {
     Serial.begin(9600);
+    Serial.println(F("setup start"));
   }
   pinMode(led, OUTPUT);
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);
+  ledsetup();
+  DEBUG_PRINT("setup complete");
 }
 
 void loop()
@@ -369,29 +412,29 @@ void loop()
     newModeAvailable = false;
 	  printBinary(newMode);
 
-    bool onOff = BIT_CHECK(newMode, 7);
-    bool saberFlickering = BIT_CHECK(newMode, 6);
-    bool rainbowMode = BIT_CHECK(newMode, 5);
-    int color = BITMASK_CHECK_ANY(newMode, 0x0F);
-
-    // switch (newMode)
-    // {
-    // case 0:
-    //   saberOff();
-    //   break;
-    // case 1:
-    //   saberOn();
-    //   saberLoop();
-    //   break;
-    // case 2:
-    //   rainbow();
-    //   break;
-    // }
+    bool onOff = BIT_CHECK(mode, 7);
+    bool newOnOff = BIT_CHECK(newMode, 7);
+    int t = BITMASK_CHECK_ANY(newMode, 0x0F);
+    DEBUG_PRINT(String(t));
+    color[0] = colors[t][0];
+    color[1] = colors[t][1];
+    color[2] = colors[t][2];
 
     mode = newMode;
+    if (newOnOff != onOff) // if on/off state has changed
+    {
+      if (newOnOff)
+        saberOn();
+      else 
+        saberOff();
+    }
+
+    if (newOnOff) // if saber is on
+      saberLoop();
   }
   else
   {
+    DEBUG_PRINT("delay");
     delay(100);
   }
 }
@@ -401,7 +444,10 @@ void receiveEvent(int howMany)
   while (Wire.available())
   {
     newMode = Wire.read();
-    newModeAvailable = true;
+    if (newMode != mode)
+    {
+      newModeAvailable = true;
+    }
   }
 }
 
